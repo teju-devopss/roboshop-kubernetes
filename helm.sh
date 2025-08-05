ed63a692b14e4bf0dd4b4815f337b8f10b82872e
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-08-05 15:46:30 +0530

    logstash configuration

--- helm.sh
+++ helm.sh
@@ -2,17 +2,21 @@ aws eks update-kubeconfig --name dev-eks
 if [ "$1" == "install" ]; then
   helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
   helm repo add elastic https://helm.elastic.co
+  helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+
 
   helm repo update
 
   helm upgrade -i ngx-ingres ingress-nginx/ingress-nginx
   kubectl apply -f external-dns.yml
-  helm install filebeat elastic/filebeat -f filebeat.yml
+  helm install -i filebeat elastic/filebeat -f filebeat.yml
+  helm install -i prometheus prometheus-community/kube-prometheus-stack -f prometheus-dev.yml
 fi
 
 if [ "$1" == "uninstall" ]; then
   helm uninstall ngx-ingres
   kubectl delete -f external-dns.yml
   helm uninstall filebeat
+  helm uninstall prometheus
 fi
 
