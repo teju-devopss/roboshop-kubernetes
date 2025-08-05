182c5351b673f1555e3160aa8ba7aa8d6596c7c2
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-08-05 11:54:22 +0530

    Create helm chart script

---
+++ helm.sh
@@ -0,0 +1,15 @@
+if [ "$1" == "install" ]; then
+  helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+  helm repo add elastic https://helm.elastic.co
+
+  helm repo update
+
+  helm upgrade -i ngx-ingres ingress-nginx/ingress-nginx
+  kubectl apply -f external-dns.yml
+  helm install filebeat elastic/filebeat -f filebeat.yml
+fi
+
+if [ "$1" == "delete" ]; then
+  helm uninstall ngx-ingres
+  kubectl delete -f external-dns.yml
+fi
